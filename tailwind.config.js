d067622e1f3229908688060991ce21fd9ced1d72
ViniciusGomesBrandao <macarraojovial@gmail.com>
2024-03-01 17:44:38 -0300

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = { 
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')], 
+  content: ['./src/**/.{html,ts}', './src/_components/**/*.{html,ts}',], 
+  theme: { 
+    extend: {}, 
+  }, 
+  plugins: [], };
